9d4393c0eb31fce0e07d0da9cae85b83647f84b2
Letitia Kernschmidt <letitia.kernschmidt@gmail.com>
2017-11-16 15:31:22 +0100

    Add relevant New York Times article links

--- js/script.js
+++ js/script.js
@@ -20,9 +20,37 @@ function loadData() {
     $greeting.text("So, you want to live at " + address + "?");
 
     var $url = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location='+ address;
-    console.log("<img class='bgimg' src=' " + $url + " '>");
     $body.append("<img class='bgimg' src=' " + $url + " '>");
 
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+      'api-key': "38730f0954124f1a8296639059a17f57",
+      'q': city
+    });
+
+    $.getJSON( url, function(result) {
+      $nytHeaderElem.text("New York Times Articles about: " + city)
+      var items = [];
+        $.each(result.response.docs, function( key, val ) {
+          items.push( "<li id='article-list'>" + "<a href=' " + val.web_url + " '>" +
+          val.headline.main + "</a><p>" + val.snippet + "</p></a></li>" );
+        });
+        items.push("<ul/>");
+        $nytElem.html(items);
+    }) .fail(function() {
+      $nytHeaderElem.text("New York Times Articles could not be loaded")
+    });
+
+    var wikiurl = "https://en.wikipedia.org/w/api.php";
+    wikiurl += '?' + $.param({
+      'action' : 'query',
+      'titles' : city,
+      'prop' : 'info',
+      'rvprop' : 'content',
+      'format' : 'json'
+    });
+    console.log("I am here!!")
+    console.log(wikiurl)
 
 
     return false;
